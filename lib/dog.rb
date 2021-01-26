b01bddd99b5692c4f102e2b4754ae00b7dd93209
Colin Schlecht <schlechtcolin@yahoo.com>
2021-01-25 22:18:49 -0800

    commit tree 7dc0eef70a459700c20751e1d5b71da71ff03d11

---
+++ lib/dog.rb
@@ -0,0 +1,53 @@
+require 'pry'
+
+#initialize method accepts a hash or keyword argument value 
+# with key-value pairs as an argument. key-value pairs need 
+# to contain id, name, and breed.
+
+class Dog
+
+    attr_accessor :name, :breed, :id
+    def initialize(id: nil, name:, breed:)
+        @id = id
+        @name = name
+        @breed = breed
+    end
+
+    def self.create_table
+        sql = <<-SQL
+        CREATE TABLE IF NOT EXISTS dogs(
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            breed TEXT
+        )
+        SQL
+        DB[:conn].execute(sql)
+    end
+
+    def self.drop_table
+        sql = <<-SQL
+        DROP TABLE dogs
+        SQL
+        DB[:conn].execute(sql)
+    end
+
+    def update
+        sql = <<-SQL
+        UPDATE dogs SET name = ?, breed = ? WHERE id = ?
+        SQL
+        DB[:conn].execute(sql, self.name, self.breed, self.id)
+    end
+
+    def save
+        # if self.id
+        #     self.update
+        # else
+            sql = <<-SQL
+            INSERT INTO dogs (name, breed)
+            VALUES (?,?)
+            SQL
+            DB[:conn].execute(sql, self.name, self.breed)
+            @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+        # end
+    end
+end
